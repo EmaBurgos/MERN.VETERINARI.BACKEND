079442d79c41a35fa76e1ccea6d740e80da22a52
Emanuel Burgos <emaburgos17@hotmail.com>
2023-07-12 19:28:14 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,19 @@ const corsOptions = {
   },
 };
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
 app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
